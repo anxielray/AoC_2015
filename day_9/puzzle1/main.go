287874c772722ea2af0480730ba4108205b0e56f
Raymond Ogwel <165130368+anxielray@users.noreply.github.com>
2024-09-12 02:04:16 +0300

    [Update]: Update source code with the solution solving the challenge 1 for day 9

--- day_9/puzzle1/main.go
+++ day_9/puzzle1/main.go
@@ -2,92 +2,75 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os"
-	"strconv"
-	"strings"
+//	"math"
 )
 
-/*
-He can start and end at any two (different) locations he wants, but he must visit each location exactly once.
-What is the shortest distance he can travel to achieve this?
-*/
+// Location distances
+var distances = map[string]map[string]int{
+	"Tristram":      {"AlphaCentauri": 34, "Snowdin": 100, "Tambi": 63, "Faerun": 108, "Norrath": 111, "Straylight": 89, "Arbre": 132},
+	"AlphaCentauri": {"Tristram": 34, "Snowdin": 4, "Tambi": 79, "Faerun": 44, "Norrath": 147, "Straylight": 133, "Arbre": 74},
+	"Snowdin":       {"Tristram": 100, "AlphaCentauri": 4, "Tambi": 105, "Faerun": 95, "Norrath": 48, "Straylight": 88, "Arbre": 7},
+	"Tambi":         {"Tristram": 63, "AlphaCentauri": 79, "Snowdin": 105, "Faerun": 68, "Norrath": 134, "Straylight": 107, "Arbre": 40},
+	"Faerun":        {"Tristram": 108, "AlphaCentauri": 44, "Snowdin": 95, "Tambi": 68, "Norrath": 11, "Straylight": 66, "Arbre": 144},
+	"Norrath":       {"Tristram": 111, "AlphaCentauri": 147, "Snowdin": 48, "Tambi": 134, "Faerun": 11, "Straylight": 115, "Arbre": 135},
+	"Straylight":    {"Tristram": 89, "AlphaCentauri": 133, "Snowdin": 88, "Tambi": 107, "Faerun": 66, "Norrath": 115, "Arbre": 127},
+	"Arbre":         {"Tristram": 132, "AlphaCentauri": 74, "Snowdin": 7, "Tambi": 40, "Faerun": 144, "Norrath": 135, "Straylight": 127},
+}
+
+// Function to find all permutations of locations
+func permute(locations []string) [][]string {
+	var result [][]string
+	var generate func([]string, int)
+	generate = func(arr []string, n int) {
+		if n == 1 {
+			tmp := make([]string, len(arr))
+			copy(tmp, arr)
+			result = append(result, tmp)
+		} else {
+			for i := 0; i < n; i++ {
+				generate(arr, n-1)
+				if n%2 == 1 {
+					arr[0], arr[n-1] = arr[n-1], arr[0]
+				} else {
+					arr[i], arr[n-1] = arr[n-1], arr[i]
+				}
+			}
+		}
+	}
+	generate(locations, len(locations))
+	return result
+}
+
+// Function to calculate the distance of a route
+func routeDistance(route []string) int {
+	totalDistance := 0
+	for i := 0; i < len(route)-1; i++ {
+		loc1, loc2 := route[i], route[i+1]
+		totalDistance += distances[loc1][loc2]
+	}
+	return totalDistance
+}
 
 func main() {
-	//extracting the location from the file...
-	file, err := os.ReadFile("../locations.txt")
-	if err != nil {
-		log.Fatal(err)
-		os.Exit(0)
+	// List of locations
+	locations := []string{"Tristram", "AlphaCentauri", "Snowdin", "Tambi", "Faerun", "Norrath", "Straylight", "Arbre"}
+
+	// Generate all permutations of locations
+	routes := permute(locations)
+
+	// Initialize variables for longest route
+	longestDistance := 0
+	var longestRoute []string
+
+	// Loop through each route and calculate the distance
+	for _, route := range routes {
+		distance := routeDistance(route)
+		if distance > longestDistance {
+			longestDistance = distance
+			longestRoute = route
+		}
 	}
-	locations := string(file)
-	createMap(locations)
-	createMapOfLocations(slice(locations))
-	// fmt.Printf("The shortest route is %v\n", computeFunctions(locations))
-}
 
-// function to compute functions together...
-// func computeFunctions(locations string) int {
-// 	var (
-// 		key      string
-// 		distance int
-// 	)
-// 	locateMap := createMap(locations)
-// 	for _, location := range strings.Split(locations, "\n") {
-// 		distance, _ = strconv.Atoi(strings.Fields(location)[4])
-// 		key = fmt.Sprintf("%s-%s", strings.Fields(location)[0], strings.Fields(location)[2])
-
-// 	}
-// 	return 0
-// }
-
-//function to loop over a string and make a map with all the strings, that are not "to" and are not "="
-//The map keys should be like "Tristram-Faerun" where the Tristram was the first in the line and the Faerun came at index 2
-func createMap(s string) map[string]int {
-	var (
-		m        = make(map[string]int)
-		splitted = strings.Split(s, "\n")
-	)
-	for _, word := range splitted {
-		value, _ := strconv.Atoi(strings.Fields(word)[4])
-		m[fmt.Sprintf("%s-%s", strings.Fields(word)[0], strings.Fields(word)[2])] = value
-	}
-	return m
-}
-
-//function to contain all the locations into the same slice...
-func slice(locations string) []string {
-	var (
-		Locations []string
-	)
-	split := strings.Split(locations, "\n")
-	for _, location := range split {
-		Locations = append(Locations, strings.Fields(location)[0])
-		Locations = append(Locations, strings.Fields(location)[2])
-	}
-	return Locations
-}
-
-//function to create a map to check fo the occurrences of the locations in the slice of strings...
-func createMapOfLocations(locations []string) map[string]int {
-	var (
-		m = make(map[string]int)
-	)
-	for _, location := range locations {
-		m[location]++
-	}
-	fmt.Println("Final map:")
-	for k, v := range m {
-		fmt.Printf("%s: %d\n", k, v)
-	}
-	return m
-}
-
-//function to extract the key from a map into a slice of string...
-func extractKeys(m map[string]int) []string {
-    var keys []string
-    for k := range m {
-        keys = append(keys, k)
-    }
-    return keys
+	// Output the result
+	fmt.Printf("The longest route is %v with a distance of %d\n", longestRoute, longestDistance)
 }
