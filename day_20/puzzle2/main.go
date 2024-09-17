a199ed1e70e5c6c47df17b3f1821f8dd16c914b2
anxielray <anxielworld@gmail.com>
2024-09-18 01:36:01 +0300

    [Add]: Add the solution to the challenge 2 of day 20

--- day_20/puzzle2/main.go
+++ day_20/puzzle2/main.go
@@ -2,27 +2,27 @@ package main
 
 import (
 	"fmt"
-	)
+)
 
-	func main() {
-		puzzleInput := 34000000
-			houseNumber := 0
-				presentsThreshold := puzzleInput / 11
-					houses := make([]int, presentsThreshold)
+func main() {
+	puzzleInput := 34000000
+	houseNumber := 0
+	presentsThreshold := puzzleInput / 11
+	houses := make([]int, presentsThreshold)
 
-						for elf := 1; elf < presentsThreshold; elf++ {
-								// Each elf delivers to at most 50 houses
-										for house := elf; house < presentsThreshold && house <= elf*50; house += elf {
-													houses[house] += elf * 11
-															}
-																}
+	for elf := 1; elf < presentsThreshold; elf++ {
+		// Each elf delivers to at most 50 houses
+		for house := elf; house < presentsThreshold && house <= elf*50; house += elf {
+			houses[house] += elf * 11
+		}
+	}
 
-																	for i := 1; i < presentsThreshold; i++ {
-																			if houses[i] >= puzzleInput {
-																						houseNumber = i
-																									break
-																											}
-																												}
+	for i := 1; i < presentsThreshold; i++ {
+		if houses[i] >= puzzleInput {
+			houseNumber = i
+			break
+		}
+	}
 
-																													fmt.Printf("The lowest house number to get at least %d presents is: %d\n", puzzleInput, houseNumber)
-																													}
+	fmt.Printf("The lowest house number to get at least %d presents is: %d\n", puzzleInput, houseNumber)
+}
